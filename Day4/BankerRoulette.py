af5bda75730ca85fffc78d08c978deb091063024
Ankush Kumar <ankushatgm1252@gmail.com>
2024-12-31 13:59:56 +0530

    BankerRoulette (Day3)

---
+++ Day4/BankerRoulette.py
@@ -0,0 +1,11 @@
+# Importing
+import random
+
+# Naming Bankers
+names = ["Bob","Alice","Hari","Shambu","Arjun","Ajeet"]
+
+#Choosing Random Business cards
+nameOfPayer = random.choice(names)
+
+#Displaying name
+print(nameOfPayer)
